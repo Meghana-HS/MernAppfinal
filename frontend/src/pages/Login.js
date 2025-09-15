622fed885050c48ded9e36d9f18966ad8e93a78d
meghana HS <hmeghana719@gmail.com>
2025-09-15 22:39:50 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = `https://mern-appfinal.vercel.app//auth/login`;
+            const url = `https://mern-appfinal-6mv9.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
